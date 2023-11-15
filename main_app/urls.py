41c3e686e649501085d9d0daca00ae014e230673
Alyssa Alonso <aalonso310@gmail.com>
2023-11-15 16:58:19 -0500

    Lab part 2 commit

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
   path('headbands/', views.headbands_index, name='index'),
+  path('headbands/<int:headband_id>/', views.headbands_detail, name='detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,5 @@
 from django.shortcuts import render
-
-headbands = [
-  {"name": "Red Plaid Minnie Mouse Ear Headband", "material": "Cloth", "description": "Minnie brings her sparkling personality to this glamorous ear headband. The striking red plaid design features a red velvet bow studded with faceted jewels and pearlescent polka dots.", "price": 35},
-  {"name": 'The Haunted Mansion "Eyes" Ear Headband', "material": "Simulated Leather", "description": "Inspired by the Disney Park attraction, it comes complete with an iconic pair of eyes acting the part of the headband's bow.", "price": 21},
-]
+from .models import Headband
 
 def home(request):
   return render(request, 'home.html')
@@ -12,6 +8,11 @@ def about(request):
   return render(request, 'about.html')
 
 def headbands_index(request):
- return render(request, 'headbands/index.html', {
-   'headbands': headbands
+  headbands = Headband.objects.all()
+  return render(request, 'headbands/index.html', {
+    'headbands': headbands
   })
+
+def headbands_detail(request, headband_id):
+  headband = Headband.objects.get(id=headband_id)
+  return render(request, 'headbands/detail.html', { 'headband': headband })
