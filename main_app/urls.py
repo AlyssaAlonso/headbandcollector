bc481830baa448f0a01ebc4ec1d15797ccb9a682
Alyssa Alonso <aalonso310@gmail.com>
2023-11-16 11:24:23 -0500

    Lab part 3 commit

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
   path('about/', views.about, name='about'),
   path('headbands/', views.headbands_index, name='index'),
   path('headbands/<int:headband_id>/', views.headbands_detail, name='detail'),
+  path('headbands/create/', views.HeadbandCreate.as_view(), name='headbands_create'),
+  path('headbands/<int:pk>/update/', views.HeadbandUpdate.as_view(), name='headbands_update'),
+  path('headbands/<int:pk>/delete/', views.HeadbandDelete.as_view(), name='headbands_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Headband
 
 def home(request):
@@ -16,3 +17,15 @@ def headbands_index(request):
 def headbands_detail(request, headband_id):
   headband = Headband.objects.get(id=headband_id)
   return render(request, 'headbands/detail.html', { 'headband': headband })
+
+class HeadbandCreate(CreateView):
+  model = Headband
+  fields = '__all__'
+
+class HeadbandUpdate(UpdateView):
+  model = Headband
+  fields = ['name', 'material', 'description', 'price']
+
+class HeadbandDelete(DeleteView):
+  model = Headband
+  success_url = '/headbands'
